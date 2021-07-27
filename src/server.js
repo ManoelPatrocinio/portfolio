e37f007bc3e3a88f4d235bc3937e6cd51b528981
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2021-07-27 18:36:19 -0300

    deploy 4 (alter port)

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const app        = express();
 require("dotenv").config();
 
-const PORT = process.env.HOST_PORT || 3333; 
+
 
 // Middleware
 app.use(express.static('public'));
@@ -43,6 +43,6 @@ app.post('/', (req,res)=>{
     })
 })
 
-app.listen(PORT,()=>{
-    console.log( `Server running on port ${PORT}`)
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("server running")
 })
