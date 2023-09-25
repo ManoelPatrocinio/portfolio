35e54412ad49bf44542b0cc4c18f422c17038b13
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-09-25 02:24:53 -0300

    projeto modelo em html,css e js

--- public/scripts/animate.js
+++ public/scripts/animate.js
@@ -1,6 +1,17 @@
-let elementList: NodeList;
+document.querySelector(".hamburguer").addEventListener("click",()=>
+   document.querySelector("#main_header").classList.toggle("show-menu")
 
-export function startAnimateOnScroll() {
+);
+
+document.querySelectorAll(".menu-link").forEach(function(btn) {
+  btn.addEventListener("click", function(){
+    document.querySelector("#main_header").classList.toggle("show-menu");
+  });
+});
+
+let elementList;
+
+function startAnimateOnScroll() {
   //get all elements with custom attributes
   elementList = document.querySelectorAll("[data-animate]");
 
@@ -10,7 +21,7 @@ export function startAnimateOnScroll() {
 
     // sets distance between element and top of the page
     const windownTop = window.pageYOffset + window.innerHeight * 0.85;
-    elementList.forEach((element: any) => {
+    elementList.forEach((element) => {
       if (windownTop > element.offsetTop) {
         element.classList.add("animate");
       } else {
@@ -25,3 +36,5 @@ export function startAnimateOnScroll() {
   });
 
 }
+startAnimateOnScroll();
+
--- src/types/projects.types.ts
+++
@@ -1,8 +0,0 @@
-export type  Project ={
-    type: "Site" | "API" | "Layouts",
-    name: string,
-    skills: string,
-    description: string,
-    link: string,
-    image: string
-}
