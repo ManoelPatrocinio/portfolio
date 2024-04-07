c372b0ebcf783d7ba1b216d39e61d09de1de8b1f
ManoelPatrocinio <47128362+ManoelPatrocinio@users.noreply.github.com>
2024-04-06 23:46:37 -0300

    debug link on logo

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/images/logo.png"
 export function NavBar() {
   return (
     <nav className="nav_menu" id="menu-desktop">
-      <a id="nav_menu__logo" href="#main-header">
+      <a id="nav_menu__logo" href="#main_header">
         <img src={Logo}alt="Logo" />
       </a>
       <ul id="nav_menu__menu">
